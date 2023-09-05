5d9b485baa953be7ee0b314b831a16780a433655
Utkarsh Gupta <utkarshgupta137@gmail.com>
2023-09-05 16:38:07 +0100

    home/neovim: add airflow python packages

--- home/programs/neovim.nix
+++ home/programs/neovim.nix
@@ -1,6 +1,16 @@
 { inputs, outputs, lib, config, pkgs, ... }: {
   programs.neovim = {
     enable = true;
+    extraPython3Packages = pyPkgs: with pkgs; [
+      (python3.pkgs.toPythonModule (apache-airflow.pythonPackages.apache-airflow.override {
+        enabledProviders = [
+          # "airflow"
+          "http"
+          # "slack"
+          "ssh"
+        ];
+      }))
+    ];
     defaultEditor = true;
     viAlias = true;
     vimAlias = true;
