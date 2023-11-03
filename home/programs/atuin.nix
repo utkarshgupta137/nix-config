bd2bb44ea747f3a5bb297a534cc0d585027ed4c3
Utkarsh Gupta <utkarshgupta137@gmail.com>
2023-11-03 19:58:12 +0000

    home/atuin: disable up arrow

--- home/programs/atuin.nix
+++ home/programs/atuin.nix
@@ -1,6 +1,9 @@
 { inputs, outputs, lib, config, pkgs, ... }: {
   programs.atuin = {
     enable = true;
+    flags = [
+      "--disable-up-arrow"
+    ];
     settings = {
       sync_frequency = "1m";
       filter_mode_shell_up_key_binding = "session";
