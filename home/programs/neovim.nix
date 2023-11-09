430300aa73b448c7002f8c4f6c5f3493a897cbc0
Utkarsh Gupta <utkarshgupta137@gmail.com>
2023-11-09 18:56:39 +0000

    home/neovim: add dprint config

--- home/programs/neovim.nix
+++ home/programs/neovim.nix
@@ -20,6 +20,10 @@
     withRuby = true;
   };
 
+  home.file.".dprint.json" = {
+    source = ../../assets/dprint.json;
+  };
+
   xdg.configFile."nvim/lua/plugins/sqlite.lua".text =
     if (pkgs.stdenv.isDarwin) then ''
       vim.g.sqlite_clib_path = '${pkgs.sqlite.out}/lib/libsqlite3.dylib'
