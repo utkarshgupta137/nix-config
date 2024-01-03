c7b6d9d730f4c3293f6b9d0570f05d9773edb790
Utkarsh Gupta <utkarshgupta137@gmail.com>
2024-01-03 13:12:00 +0000

    home/alacritty: key binding updates

--- home/programs/alacritty.nix
+++ home/programs/alacritty.nix
@@ -13,42 +13,38 @@
         italic.style = "Medium Italic";
 
         size = if (pkgs.stdenv.isDarwin) then 13 else 6;
-
-        builtin_box_drawing = true;
       };
 
-      draw_bold_text_with_bright_colors = true;
+      colors.draw_bold_text_with_bright_colors = true;
 
       selection.save_to_clipboard = true;
 
       mouse.hide_when_typing = true;
 
-      key_bindings = [
-        { key = "Key1"; mods = "Control"; chars = "\\x1b!"; }
-        { key = "Key2"; mods = "Control"; chars = "\\x1b@"; }
-        { key = "Key3"; mods = "Control"; chars = "\\x1b#"; }
-        { key = "Key4"; mods = "Control"; chars = "\\x1b$"; }
-        { key = "Key5"; mods = "Control"; chars = "\\x1b%"; }
-        { key = "Key6"; mods = "Control"; chars = "\\x1b^"; }
-        { key = "Key7"; mods = "Control"; chars = "\\x1b&"; }
-        { key = "Key8"; mods = "Control"; chars = "\\x1b*"; }
-        { key = "Key9"; mods = "Control"; chars = "\\x1b("; }
-        { key = "Key0"; mods = "Control"; chars = "\\x1b)"; }
+      keyboard.bindings = [
+        { key = "Key1"; mods = "Control"; chars = "\\u001B!"; }
+        { key = "Key2"; mods = "Control"; chars = "\\u001B@"; }
+        { key = "Key3"; mods = "Control"; chars = "\\u001B#"; }
+        { key = "Key4"; mods = "Control"; chars = "\\u001B$"; }
+        { key = "Key5"; mods = "Control"; chars = "\\u001B%"; }
+        { key = "Key6"; mods = "Control"; chars = "\\u001B^"; }
+        { key = "Key7"; mods = "Control"; chars = "\\u001B&"; }
+        { key = "Key8"; mods = "Control"; chars = "\\u001B*"; }
+        { key = "Key9"; mods = "Control"; chars = "\\u001B("; }
+        { key = "Key0"; mods = "Control"; chars = "\\u001B)"; }
 
-        { key = "T"; mods = "Command"; chars = "\\x1bT"; }
-        { key = "N"; mods = "Command"; chars = "\\x1bN"; }
-        { key = "T"; mods = "Command|Shift"; action = "CreateNewWindow"; }
-        { key = "N"; mods = "Command|Shift"; action = "SpawnNewInstance"; }
+        { key = "T"; mods = "Command"; chars = "\\u001BT"; }
+        { key = "N"; mods = "Command"; chars = "\\u001BN"; }
+        { key = "T"; mods = "Command|Shift"; action = "CreateNewTab"; }
+        { key = "N"; mods = "Command|Shift"; action = "CreateNewWindow"; }
 
         # Ref: https://stackoverflow.com/questions/16359878/how-to-map-shift-enter
-        { key = "Return"; mods = "Shift"; chars = "\\x1b[13;2u"; }
-        { key = "Return"; mods = "Control"; chars = "\\x1b[13;5u"; }
-        { key = "Return"; mods = "Control|Shift"; chars = "\\x1b[13;6u"; }
+        { key = "Return"; mods = "Shift"; chars = "\\u001B[13;2u"; }
+        { key = "Return"; mods = "Control"; chars = "\\u001B[13;5u"; }
+        { key = "Return"; mods = "Control|Shift"; chars = "\\u001B[13;6u"; }
       ] ++ lib.optionals (pkgs.stdenv.isDarwin) [
-        { key = "LBracket"; mods = "Command"; chars = "\\x1bj"; }
-        { key = "RBracket"; mods = "Command"; chars = "\\x1bk"; }
-        { key = "LBracket"; mods = "Command|Shift"; chars = "\\x1bh"; }
-        { key = "RBracket"; mods = "Command|Shift"; chars = "\\x1bl"; }
+        { key = "H"; mods = "Alt|Shift"; action = "SelectPreviousTab"; }
+        { key = "L"; mods = "Alt|Shift"; action = "SelectNextTab"; }
 
         { key = "Q"; mods = "Command"; action = "None"; }
         { key = "W"; mods = "Command"; action = "None"; }
