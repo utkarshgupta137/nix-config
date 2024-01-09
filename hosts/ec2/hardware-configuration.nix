7a8b65a904321b24d85f0e35b28f2a4c4d1c8095
Utkarsh Gupta <utkarshgupta137@gmail.com>
2024-01-09 13:41:02 +0000

    hosts/ec2: add '/data' mount for NVMe

--- hosts/ec2/hardware-configuration.nix
+++ hosts/ec2/hardware-configuration.nix
@@ -2,4 +2,12 @@
   boot = {
     kernelParams = [ "nosmt" "isolcpus=0-19" ];
   };
+
+  fileSystems = {
+    "/data" = {
+      autoFormat = true;
+      device = "/dev/disk/by-path/pci-0000:00:1f.0-nvme-1";
+      fsType = "xfs";
+    };
+  };
 }
