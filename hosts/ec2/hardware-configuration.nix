3ba265f206b8e80311af100849a6b14f62ab298e
Utkarsh Gupta <utkarshgupta137@gmail.com>
2024-11-07 13:50:59 +0000

    hosts/ec2: remove /data partition

--- hosts/ec2/hardware-configuration.nix
+++ hosts/ec2/hardware-configuration.nix
@@ -12,12 +12,4 @@
     kernelPackages = pkgs.linuxPackages_latest;
     kernelParams = [ "nosmt" ];
   };
-
-  fileSystems = {
-    "/data" = {
-      autoFormat = true;
-      device = "/dev/disk/by-path/pci-0000:00:1f.0-nvme-1";
-      fsType = "xfs";
-    };
-  };
 }
