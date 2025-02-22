594d30c84b9c579c97524cf88b5fc670413cfc09
Utkarsh Gupta <utkarshgupta137@gmail.com>
2025-02-22 02:44:00 +0000

    hosts/darwin: swap Alt & Meta

--- hosts/services/evremap.nix
+++ hosts/services/evremap.nix
@@ -13,16 +13,27 @@
     settings = {
       device_name = "ITE Tech. Inc. ITE Device(8258) Keyboard";
 
+      remap = [
+        {
+          input = [ "KEY_LEFTALT" ];
+          output = [ "KEY_LEFTMETA" ];
+        }
+        {
+          input = [ "KEY_LEFTMETA" ];
+          output = [ "KEY_LEFTALT" ];
+        }
+      ];
+
       dual_role = [
         {
           input = "KEY_CAPSLOCK";
           tap = [ "KEY_ESC" ];
-          hold = [ "KEY_LEFTCTRL" ];
+          hold = [ "KEY_RIGHTCTRL" ];
         }
         {
           input = "KEY_TAB";
           tap = [ "KEY_TAB" ];
-          hold = [ "KEY_LEFTALT" ];
+          hold = [ "KEY_RIGHTALT" ];
         }
       ];
     };
