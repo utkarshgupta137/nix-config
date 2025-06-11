6134561106b975ad7e2f79a0a30a73071d8cbf37
Utkarsh Gupta <utkarshgupta137@gmail.com>
2025-06-11 21:29:19 +0100

    hosts/ec2: add SO_TIMESTAMP support to tls module

--- hosts/ec2/hardware-configuration.nix
+++ hosts/ec2/hardware-configuration.nix
@@ -6,10 +6,22 @@
   pkgs,
   ...
 }:
+
+let
+  tls-kernel-module = pkgs.callPackage ../../overlays/tls_so_timestamp.nix {
+    # Make sure the module targets the same kernel as your system is using.
+    kernel = config.boot.kernelPackages.kernel;
+  };
+in
 {
   boot = {
     kernelModules = [ "tls" ];
     kernelPackages = pkgs.linuxPackages_latest;
     kernelParams = [ "nosmt" ];
+    extraModulePackages = [
+      (tls-kernel-module.overrideAttrs (_: {
+        patches = [ ../../overlays/tls_so_timestamp.patch ];
+      }))
+    ];
   };
 }
