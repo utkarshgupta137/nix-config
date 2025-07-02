2a5cc95d2e09464aba681c9a2b18c3b180ff81b9
Utkarsh Gupta <utkarshgupta137@gmail.com>
2025-07-02 11:24:47 +0000

    hosts/ec2: enable phc

--- hosts/ec2/hardware-configuration.nix
+++ hosts/ec2/hardware-configuration.nix
@@ -10,6 +10,19 @@
   boot = {
     kernelModules = [ "tls" ];
     kernelPackages = pkgs.linuxPackages_latest;
-    kernelParams = [ "nosmt" ];
+    kernelParams = [
+      "ena.lpc_size=32"
+      "ena.num_io_queues=1"
+      "ena.phc_enable=1"
+      "ena.rx_queue_size=16384"
+      "isolcpus=16-31"
+      "mitigations=off"
+    ];
+  };
+
+  services.chrony = {
+    extraConfig = "
+      refclock PHC /dev/ptp0 poll 0 delay 0.000010 prefer
+    ";
   };
 }
