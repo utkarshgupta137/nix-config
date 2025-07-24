1e7f072cfbaa176ec51dc6937bd0d14947fde331
Utkarsh Gupta <utkarshgupta137@gmail.com>
2025-07-24 11:31:24 +0100

    home: nixfmt

--- home/programs/alacritty.nix
+++ home/programs/alacritty.nix
@@ -29,147 +29,146 @@
 
       mouse.hide_when_typing = if (pkgs.stdenv.isDarwin) then true else false;
 
-      keyboard.bindings =
-        [
-          {
-            key = "Key1";
-            mods = "Control";
-            chars = "\\u001B!";
-          }
-          {
-            key = "Key2";
-            mods = "Control";
-            chars = "\\u001B@";
-          }
-          {
-            key = "Key3";
-            mods = "Control";
-            chars = "\\u001B#";
-          }
-          {
-            key = "Key4";
-            mods = "Control";
-            chars = "\\u001B$";
-          }
-          {
-            key = "Key5";
-            mods = "Control";
-            chars = "\\u001B%";
-          }
-          {
-            key = "Key6";
-            mods = "Control";
-            chars = "\\u001B^";
-          }
-          {
-            key = "Key7";
-            mods = "Control";
-            chars = "\\u001B&";
-          }
-          {
-            key = "Key8";
-            mods = "Control";
-            chars = "\\u001B*";
-          }
-          {
-            key = "Key9";
-            mods = "Control";
-            chars = "\\u001B(";
-          }
-          {
-            key = "Key0";
-            mods = "Control";
-            chars = "\\u001B)";
-          }
+      keyboard.bindings = [
+        {
+          key = "Key1";
+          mods = "Control";
+          chars = "\\u001B!";
+        }
+        {
+          key = "Key2";
+          mods = "Control";
+          chars = "\\u001B@";
+        }
+        {
+          key = "Key3";
+          mods = "Control";
+          chars = "\\u001B#";
+        }
+        {
+          key = "Key4";
+          mods = "Control";
+          chars = "\\u001B$";
+        }
+        {
+          key = "Key5";
+          mods = "Control";
+          chars = "\\u001B%";
+        }
+        {
+          key = "Key6";
+          mods = "Control";
+          chars = "\\u001B^";
+        }
+        {
+          key = "Key7";
+          mods = "Control";
+          chars = "\\u001B&";
+        }
+        {
+          key = "Key8";
+          mods = "Control";
+          chars = "\\u001B*";
+        }
+        {
+          key = "Key9";
+          mods = "Control";
+          chars = "\\u001B(";
+        }
+        {
+          key = "Key0";
+          mods = "Control";
+          chars = "\\u001B)";
+        }
 
-          # Ref: https://stackoverflow.com/questions/16359878/how-to-map-shift-enter
-          {
-            key = "Return";
-            mods = "Shift";
-            chars = "\\u001B[13;2u";
-          }
-          {
-            key = "Return";
-            mods = "Control";
-            chars = "\\u001B[13;5u";
-          }
-          {
-            key = "Return";
-            mods = "Control|Shift";
-            chars = "\\u001B[13;6u";
-          }
-        ]
-        ++ lib.optionals (pkgs.stdenv.isDarwin) [
-          {
-            key = "H";
-            mods = "Alt|Shift";
-            action = "SelectPreviousTab";
-          }
-          {
-            key = "L";
-            mods = "Alt|Shift";
-            action = "SelectNextTab";
-          }
+        # Ref: https://stackoverflow.com/questions/16359878/how-to-map-shift-enter
+        {
+          key = "Return";
+          mods = "Shift";
+          chars = "\\u001B[13;2u";
+        }
+        {
+          key = "Return";
+          mods = "Control";
+          chars = "\\u001B[13;5u";
+        }
+        {
+          key = "Return";
+          mods = "Control|Shift";
+          chars = "\\u001B[13;6u";
+        }
+      ]
+      ++ lib.optionals (pkgs.stdenv.isDarwin) [
+        {
+          key = "H";
+          mods = "Alt|Shift";
+          action = "SelectPreviousTab";
+        }
+        {
+          key = "L";
+          mods = "Alt|Shift";
+          action = "SelectNextTab";
+        }
 
-          {
-            key = "Q";
-            mods = "Command";
-            action = "None";
-          }
-          {
-            key = "W";
-            mods = "Command";
-            action = "None";
-          }
+        {
+          key = "Q";
+          mods = "Command";
+          action = "None";
+        }
+        {
+          key = "W";
+          mods = "Command";
+          action = "None";
+        }
 
-          {
-            key = "T";
-            mods = "Command";
-            chars = "\\u001BT";
-          }
-          {
-            key = "N";
-            mods = "Command";
-            chars = "\\u001BN";
-          }
-          {
-            key = "T";
-            mods = "Command|Shift";
-            action = "CreateNewTab";
-          }
-          {
-            key = "N";
-            mods = "Command|Shift";
-            action = "CreateNewWindow";
-          }
-        ]
-        ++ lib.optionals (pkgs.stdenv.isLinux) [
-          {
-            key = "C";
-            mods = "Super";
-            action = "Copy";
-          }
-          {
-            key = "V";
-            mods = "Super";
-            action = "Paste";
-          }
-          {
-            key = "T";
-            mods = "Super";
-            chars = "\\u001BT";
-          }
-          {
-            key = "N";
-            mods = "Super";
-            chars = "\\u001BN";
-          }
-          {
-            key = "N";
-            mods = "Super|Shift";
-            action = "CreateNewWindow";
-          }
-        ];
+        {
+          key = "T";
+          mods = "Command";
+          chars = "\\u001BT";
+        }
+        {
+          key = "N";
+          mods = "Command";
+          chars = "\\u001BN";
+        }
+        {
+          key = "T";
+          mods = "Command|Shift";
+          action = "CreateNewTab";
+        }
+        {
+          key = "N";
+          mods = "Command|Shift";
+          action = "CreateNewWindow";
+        }
+      ]
+      ++ lib.optionals (pkgs.stdenv.isLinux) [
+        {
+          key = "C";
+          mods = "Super";
+          action = "Copy";
+        }
+        {
+          key = "V";
+          mods = "Super";
+          action = "Paste";
+        }
+        {
+          key = "T";
+          mods = "Super";
+          chars = "\\u001BT";
+        }
+        {
+          key = "N";
+          mods = "Super";
+          chars = "\\u001BN";
+        }
+        {
+          key = "N";
+          mods = "Super|Shift";
+          action = "CreateNewWindow";
+        }
+      ];
     };
   };
 }
--- home/programs/htop.nix
+++ home/programs/htop.nix
@@ -10,50 +10,49 @@
   programs.htop = {
     enable = true;
     package = pkgs.htop-vim;
-    settings =
-      {
-        delay = 10;
-        fields = with config.lib.htop.fields; [
-          PID
-          PRIORITY
-          NICE
-          USER
-          M_SIZE
-          M_RESIDENT
-          M_SHARE
-          PERCENT_CPU
-          PERCENT_MEM
-          TIME
-          COMM
-        ];
-        highlight_base_name = true;
-        highlight_changes = true;
-        hide_userland_threads = false;
-        shadow_distribution_path_prefix = true;
-        shadow_other_users = true;
-        show_thread_names = true;
-        tree_view = true;
-        tree_sort_key = config.lib.htop.fields.COMM;
-      }
-      // (
-        with config.lib.htop;
-        leftMeters [
-          (bar "LeftCPUs2")
-          (text "Blank")
-          (bar "CPU")
-          (bar "Memory")
-          (bar "Swap")
-        ]
-      )
-      // (
-        with config.lib.htop;
-        rightMeters [
-          (bar "RightCPUs2")
-          (text "Blank")
-          (text "Tasks")
-          (text "LoadAverage")
-          (text "Uptime")
-        ]
-      );
+    settings = {
+      delay = 10;
+      fields = with config.lib.htop.fields; [
+        PID
+        PRIORITY
+        NICE
+        USER
+        M_SIZE
+        M_RESIDENT
+        M_SHARE
+        PERCENT_CPU
+        PERCENT_MEM
+        TIME
+        COMM
+      ];
+      highlight_base_name = true;
+      highlight_changes = true;
+      hide_userland_threads = false;
+      shadow_distribution_path_prefix = true;
+      shadow_other_users = true;
+      show_thread_names = true;
+      tree_view = true;
+      tree_sort_key = config.lib.htop.fields.COMM;
+    }
+    // (
+      with config.lib.htop;
+      leftMeters [
+        (bar "LeftCPUs2")
+        (text "Blank")
+        (bar "CPU")
+        (bar "Memory")
+        (bar "Swap")
+      ]
+    )
+    // (
+      with config.lib.htop;
+      rightMeters [
+        (bar "RightCPUs2")
+        (text "Blank")
+        (text "Tasks")
+        (text "LoadAverage")
+        (text "Uptime")
+      ]
+    );
   };
 }
