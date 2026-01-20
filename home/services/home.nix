6216162106fcadb944fcb33ae813e9f89bd742ec
Utkarsh Gupta <utkarshgupta137@gmail.com>
2026-01-20 14:39:16 +0000

    home: remove unused packages

--- home/services/home.nix
+++ home/services/home.nix
@@ -15,41 +15,21 @@
     packages =
       with pkgs;
       [
-        awscli2
         cmake
-        go-migrate
-        grafana-loki
-        neovim-remote
-        ssm-session-manager-plugin
-        zstd
 
-        bandwhich
-        bmon
-        dprint
-        just
-        nixfmt-rfc-style
-        rm-improved
         sccache
         sd
         tokei
         tree-sitter
         uutils-coreutils-noprefix
         vivid
-        websocat
 
         rustup
-        cargo-audit
-        cargo-cranky
-        cargo-criterion
-        cargo-deny
-        cargo-hack
         cargo-machete
-        cargo-nextest
         cargo-pgo
         cargo-shear
         cargo-show-asm
         cargo-sweep
-        cargo-watch
       ]
       ++ lib.optionals (pkgs.stdenv.isDarwin) [
         pinentry_mac
@@ -71,14 +51,9 @@
     preferXdgDirectories = true;
 
     sessionVariables = {
-      AWS_CLI_HISTORY_FILE = "${config.xdg.stateHome}/aws/history";
-      AWS_CONFIG_FILE = "${config.xdg.configHome}/aws/config";
       CARGO_HOME = "${config.xdg.dataHome}/cargo";
       CARGO_NET_GIT_FETCH_WITH_CLI = "true";
       CARGO_TARGET_DIR = "${config.xdg.cacheHome}/cargo";
-      DPRINT_CACHE_DIR = "${config.xdg.cacheHome}/dprint";
-      GOPATH = "${config.xdg.dataHome}/go";
-      GRANTED_ALIAS_CONFIGURED = "true";
       IPYTHONDIR = "${config.xdg.dataHome}/ipython";
       LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib:${pkgs.zlib}/lib";
       LESSHISTFILE = "${config.xdg.stateHome}/less/history";
@@ -103,8 +78,6 @@
       j = "zellij";
       jr = "zellij -l rsky attach -c rsky options --no-pane-frames";
       jcf = "zellij -l config attach -c config options --no-pane-frames";
-      jfx = "zellij -l tfx attach -c tfx options --no-pane-frames";
-      jtf = "zellij -l tf attach -c tf";
     };
 
     # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
