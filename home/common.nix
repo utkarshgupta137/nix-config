c2c868fd276b36a8a23f2d60b1aa566b33e15d75
Utkarsh Gupta <utkarshgupta137@gmail.com>
2026-01-20 11:36:40 +0000

    home: remove unused programs

--- home/common.nix
+++ home/common.nix
@@ -11,26 +11,16 @@
     # ./programs/alacritty.nix
     ./programs/atuin.nix
     ./programs/bat.nix
-    ./programs/bottom.nix
-    ./programs/broot.nix
     ./programs/delta.nix
     ./programs/fd.nix
     ./programs/fzf.nix
-    ./programs/gh.nix
-    ./programs/gitui.nix
-    ./programs/go.nix
-    ./programs/granted.nix
-    ./programs/helix.nix
     ./programs/htop.nix
     ./programs/jq.nix
     ./programs/lazygit.nix
     ./programs/lsd.nix
-    ./programs/navi.nix
     ./programs/neovim.nix
-    ./programs/obsidian.nix
     ./programs/ripgrep.nix
     ./programs/starship.nix
-    ./programs/tealdeer.nix
     ./programs/uv.nix
     ./programs/zellij.nix
     ./programs/zoxide.nix
