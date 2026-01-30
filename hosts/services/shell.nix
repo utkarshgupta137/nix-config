b594eaf171a491781f62572442bbcd875838acb1
Utkarsh Gupta <utkarshgupta137@gmail.com>
2026-01-30 16:07:40 +0000

    hosts/shell: add XDG_BIN_HOME to PATH

--- hosts/services/shell.nix
+++ hosts/services/shell.nix
@@ -7,43 +7,53 @@
   ...
 }:
 {
-  environment = {
-    etc.pythonrc.text = /* python */ ''
-      def is_vanilla() -> bool:
-          import sys
-          return not hasattr(__builtins__, '__IPYTHON__') and 'bpython' not in sys.argv[0]
+  environment = lib.mkMerge [
+    {
+      etc.pythonrc.text = /* python */ ''
+        def is_vanilla() -> bool:
+            import sys
+            return not hasattr(__builtins__, '__IPYTHON__') and 'bpython' not in sys.argv[0]
 
 
-      def setup_history():
-          import os
-          import atexit
-          import readline
-          from pathlib import Path
+        def setup_history():
+            import os
+            import atexit
+            import readline
+            from pathlib import Path
 
-          if state_home := os.environ.get('XDG_STATE_HOME'):
-              state_home = Path(state_home)
-          else:
-              state_home = Path.home() / '.local' / 'state'
+            if state_home := os.environ.get('XDG_STATE_HOME'):
+                state_home = Path(state_home)
+            else:
+                state_home = Path.home() / '.local' / 'state'
 
-          history: Path = state_home / 'python' / 'history'
+            history: Path = state_home / 'python' / 'history'
 
-          readline.read_history_file(str(history))
-          atexit.register(readline.write_history_file, str(history))
+            readline.read_history_file(str(history))
+            atexit.register(readline.write_history_file, str(history))
 
 
-      if is_vanilla():
-          setup_history()
-    '';
+        if is_vanilla():
+            setup_history()
+      '';
 
-    profiles = [ "\$HOME/.local/state/nix/profile" ];
+      profiles = [ "\$HOME/.local/state/nix/profile" ];
 
-    shells = [ pkgs.fish ];
+      shells = [ pkgs.fish ];
 
-    variables = {
-      HISTFILE = "\$HOME/.local/state/bash/history";
-      PYTHONSTARTUP = "/etc/pythonrc";
-    };
-  };
+      variables = {
+        HISTFILE = "\$HOME/.local/state/bash/history";
+        PYTHONSTARTUP = "/etc/pythonrc";
+      };
+    }
+
+    (lib.optionalAttrs pkgs.stdenv.isDarwin {
+      systemPath = [ "\$HOME/.local/bin" ];
+    })
+
+    (lib.optionalAttrs pkgs.stdenv.isLinux {
+      localBinInPath = true;
+    })
+  ];
 
   programs.fish = {
     enable = true;
