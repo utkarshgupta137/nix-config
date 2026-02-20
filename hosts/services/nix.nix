1a9c5ac9ebca318e6d8c9b2e731352a4543fcec0
Utkarsh Gupta <utkarshgupta137@gmail.com>
2026-02-20 10:50:11 +0000

    Revert "hosts/nix: add trusted-users"
    This reverts commit a08beb0a2e82414c10a8bb07ea4d7e5355c3e3f1.

--- hosts/services/nix.nix
+++ hosts/services/nix.nix
@@ -37,11 +37,6 @@
             "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
           ];
 
-          trusted-users = [
-            "root"
-            "@wheel"
-          ];
-
           use-xdg-base-directories = true;
 
           warn-dirty = false;
