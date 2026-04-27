1d1ac98e83b8c004cd313277550c6bf760fe0928
Utkarsh Gupta <utkarshgupta137@gmail.com>
2026-04-27 13:32:31 +0100

    home/alacritty: use <mod>+<digit> to switch zellij tabs

--- home/programs/alacritty.nix
+++ home/programs/alacritty.nix
@@ -48,135 +48,109 @@
 
       mouse.hide_when_typing = if pkgs.stdenv.isDarwin then true else false;
 
-      keyboard.bindings = [
-        {
-          key = "Key1";
-          mods = "Control";
-          chars = "\\u001B!";
-        }
-        {
-          key = "Key2";
-          mods = "Control";
-          chars = "\\u001B@";
-        }
-        {
-          key = "Key3";
-          mods = "Control";
-          chars = "\\u001B#";
-        }
-        {
-          key = "Key4";
-          mods = "Control";
-          chars = "\\u001B$";
-        }
-        {
-          key = "Key5";
-          mods = "Control";
-          chars = "\\u001B%";
-        }
-        {
-          key = "Key6";
-          mods = "Control";
-          chars = "\\u001B^";
-        }
-        {
-          key = "Key7";
-          mods = "Control";
-          chars = "\\u001B&";
-        }
-        {
-          key = "Key8";
-          mods = "Control";
-          chars = "\\u001B*";
-        }
-        {
-          key = "Key9";
-          mods = "Control";
-          chars = "\\u001B(";
-        }
-        {
-          key = "Key0";
-          mods = "Control";
-          chars = "\\u001B)";
-        }
+      keyboard.bindings =
+        let
+          digits = [
+            "1"
+            "2"
+            "3"
+            "4"
+            "5"
+            "6"
+            "7"
+            "8"
+            "9"
+            "0"
+          ];
+          # Remap <mod>+<digit> -> Alt+<digit>
+          digitBindings = map (n: {
+            key = "Key${n}";
+            mods = if pkgs.stdenv.isDarwin then "Command" else "Control";
+            chars = "\\u001B${n}";
+          }) digits;
+        in
+        digitBindings
+        ++ [
+          {
+            key = "Return";
+            mods = "Shift";
+            chars = "\\u001B\\r";
+          }
+        ]
+        ++ lib.optionals pkgs.stdenv.isDarwin [
+          {
+            key = "T";
+            mods = "Command";
+            chars = "\\u001BT";
+          }
+          {
+            key = "N";
+            mods = "Command";
+            chars = "\\u001BN";
+          }
 
-        {
-          key = "Return";
-          mods = "Shift";
-          chars = "\\u001B\\r";
-        }
-      ]
-      ++ lib.optionals pkgs.stdenv.isDarwin [
-        {
-          key = "H";
-          mods = "Alt|Shift";
-          action = "SelectPreviousTab";
-        }
-        {
-          key = "L";
-          mods = "Alt|Shift";
-          action = "SelectNextTab";
-        }
+          {
+            key = "T";
+            mods = "Command|Shift";
+            action = "CreateNewTab";
+          }
+          {
+            key = "N";
+            mods = "Command|Shift";
+            action = "CreateNewWindow";
+          }
 
-        {
-          key = "Q";
-          mods = "Command";
-          action = "None";
-        }
-        {
-          key = "W";
-          mods = "Command";
-          action = "None";
-        }
+          {
+            key = "H";
+            mods = "Alt|Shift";
+            action = "SelectPreviousTab";
+          }
+          {
+            key = "L";
+            mods = "Alt|Shift";
+            action = "SelectNextTab";
+          }
 
-        {
-          key = "T";
-          mods = "Command";
-          chars = "\\u001BT";
-        }
-        {
-          key = "N";
-          mods = "Command";
-          chars = "\\u001BN";
-        }
-        {
-          key = "T";
-          mods = "Command|Shift";
-          action = "CreateNewTab";
-        }
-        {
-          key = "N";
-          mods = "Command|Shift";
-          action = "CreateNewWindow";
-        }
-      ]
-      ++ lib.optionals pkgs.stdenv.isLinux [
-        {
-          key = "C";
-          mods = "Super";
-          action = "Copy";
-        }
-        {
-          key = "V";
-          mods = "Super";
-          action = "Paste";
-        }
-        {
-          key = "T";
-          mods = "Super";
-          chars = "\\u001BT";
-        }
-        {
-          key = "N";
-          mods = "Super";
-          chars = "\\u001BN";
-        }
-        {
-          key = "N";
-          mods = "Super|Shift";
-          action = "CreateNewWindow";
-        }
-      ];
+          {
+            key = "W";
+            mods = "Command";
+            action = "None";
+          }
+          {
+            key = "Q";
+            mods = "Command";
+            action = "None";
+          }
+        ]
+        ++ lib.optionals pkgs.stdenv.isLinux [
+          {
+            key = "T";
+            mods = "Super";
+            chars = "\\u001BT";
+          }
+          {
+            key = "N";
+            mods = "Super";
+            chars = "\\u001BN";
+          }
+
+          {
+            key = "N";
+            mods = "Super|Shift";
+            action = "CreateNewWindow";
+          }
+
+          {
+            key = "C";
+            mods = "Super";
+            action = "Copy";
+          }
+          {
+            key = "V";
+            mods = "Super";
+            action = "Paste";
+          }
+        ];
     };
   };
 }
